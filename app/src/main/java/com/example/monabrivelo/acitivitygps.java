46ddfc5d0d9160e2bf91d526cea3d76d8e327665
NagaShadow <julien.gicquel511@gmail.com>
2019-12-20 17:47:39 +0100

    boutons appliquer manques quelque uns

---
+++ app/src/main/java/com/example/monabrivelo/acitivitygps.java
@@ -0,0 +1,14 @@
+package com.example.monabrivelo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class acitivitygps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acitivitygps);
+    }
+}
